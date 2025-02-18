3ac691c461dd6d98c768290d9db8499c1396675e
Ajinkya Jacob <ajinkyajacob@gmail.com>
2025-02-18 19:06:10 +0530

    backend and frontend changes

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Apollo, gql } from "apollo-angular";
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  apollo = inject(Apollo)
+
+  register(payload: {username:string; password: string}){
+    return this.apollo.watchQuery<{jwt:string}>({
+      query: gql `query{
+        jwt
+      }`,
+      context: payload
+    }).valueChanges.pipe(tap(({data}) => localStorage.setItem('jwt',data.jwt)))
+  }
+
+
+  login(payload: {username:string; password: string}){
+    return this.apollo.watchQuery<{jwt:string}>({
+      query: gql `query{
+        jwt
+      }`,
+      context: payload
+    }).valueChanges.pipe(tap(({data}) => localStorage.setItem('jwt',data.jwt)))
+  }
+
+  logout(){
+    localStorage.clear()
+  }
 }
